f857eb649af10b87b9b279b3d722dc448378867c
minosss <minoscc+dev@gmail.com>
2022-07-14 22:27:56 +0800

    chore: copy system hosts at the first initial

--- src/utils/install.ts
+++ src/utils/install.ts
@@ -1,5 +1,8 @@
 import {defaultConfig} from './config';
 import {ensureAppDir, readAppFile, writeAppFile} from './fs';
+import {uuid} from './helper';
+import {writeHostFile} from './hosts';
+import {readSystemHost} from './system-hosts';
 
 export async function isInstalled() {
 	try {
@@ -13,10 +16,31 @@ export async function isInstalled() {
 export async function install() {
 	if (await isInstalled()) return;
 
+	// appDir/data
 	await ensureAppDir();
 	// store app config
 	await writeAppFile('config.json', JSON.stringify(defaultConfig, null, 2));
+
+	// copy system hosts
+	const id = uuid();
+	const content = await readSystemHost();
+	await writeHostFile(id, content);
+
 	// array of hosts
-	await writeAppFile('hosts.json', JSON.stringify([], null, 2));
+	await writeAppFile(
+		'hosts.json',
+		JSON.stringify(
+			[
+				{
+					id,
+					kind: 'local',
+					name: 'System Copy',
+					enable: false,
+				},
+			],
+			null,
+			2
+		)
+	);
 	await writeAppFile('install.lock', `${Date.now()}`);
 }
